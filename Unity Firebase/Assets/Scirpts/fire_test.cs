04d7b7ea9172bc185768533cb6adc236d9f9844b
Neel Bhave <neelbhave91@gmail.com>
2017-09-30 11:59:20 +0530

    Initial Commit

---
+++ Unity Firebase/Assets/Scirpts/fire_test.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine.UI;
+
+public class fire_test : MonoBehaviour {
+
+    DatabaseReference reference;
+	// Use this for initialization
+	void Start () {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://unity-firebase-5a873.firebaseio.com/");
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        
+    }
+	
+	public void AddData(InputField name)
+    {
+        User newuser = new User(name.text.ToString());
+        string json = JsonUtility.ToJson(newuser);
+        reference.Child("Users").Push().SetValueAsync(json);
+    }
+        
+
+    public void RefreshData()
+    {
+        
+        FirebaseDatabase.DefaultInstance.GetReference("Users").OrderByChild("name").EqualTo("neel").GetValueAsync().ContinueWith(
+            task =>
+            {
+                DataSnapshot snapshot = task.Result;
+                var result = snapshot.Value as Dictionary<string, object>;
+                foreach (var item in result)
+                {
+                    Debug.Log(item.Key); // Kdq6...
+                    var values = JsonUtility.FromJson<User>(item.Value.ToString());
+                    Debug.Log(values.name + " " + values.n);
+
+                    //foreach (var v in values)
+                    //{
+                    //    Debug.Log(v.Key + ":" + v.Value); // category:livingroom, code:126 ...
+                    //}
+                }
+            }
+
+            );
+
+      //  var newval = FirebaseDatabase.DefaultInstance.GetReference("Users").OrderByChild("name").EqualTo("Neel");
+      
+    }
+}
+
+
+
+
+
+
+
+public class User
+{
+    public string name;
+    public int n = 2;
+
+    public User(string newName)
+    {
+        this.name = newName;
+    }
+}
